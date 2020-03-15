8753d30931447dd27b5a461fa42885332c9e30e1
pulsecode <12067255+dfirsec@users.noreply.github.com>
2020-03-15 13:21:18 -0400

    Update termcolors.py

--- termcolors.py
+++ termcolors.py
@@ -10,6 +10,7 @@ class Termcolor:
     CYAN = Fore.CYAN + Style.BRIGHT
     GREEN = Fore.GREEN
     GRAY = Fore.BLACK + Style.BRIGHT
+    YELLOW = Fore.LIGHTYELLOW_EX
     WARNING = Fore.YELLOW + "\u0021" + Style.RESET_ALL
     ERROR = Fore.RED + "\u2718" + Style.RESET_ALL
     RESET = Style.RESET_ALL
